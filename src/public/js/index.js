fe68dca2269287b0df5cadde10b589d65ba2d81e
한중명 <gkswndaud@gmail.com>
2023-01-11 17:13:18 +0900

    install socket.io

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,47 +1,63 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message"); //html의 기능을 재정의
+/**************************/
+/*        SocektIO        */
+/**************************/
+const socket = io();
 
-const socket = new WebSocket(`ws\://${window.location.host}`); //브라우저에서 지원하는 웹소켓
-//localhost:3000 => window.location.host 모바일을 지원하기 위해 변경
-//연결할 서버 소켓
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-//소켓으로 보낸 메시지의 성격을 구분하기 위해 이벤트명을 작성
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-  // websocket API가 브라우저에서 제공하는 것이기 때문에, 환경을 유연하게 둬야함
-  //응답을 받는 서버가 통일되지 않은 상태 즉, js서버가 아닐 수 있기에, string으로 보내야 처리가 가능
-}
-
-socket.addEventListener("open", () => {
-  console.log("Connected to Server V");
-}); //server와 연결을 시작
-
-socket.addEventListener("message", (message) => {
-  console.log(message);
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-  console.log("Disconnected to Server V");
-}); //server와 연결을 시작
-
-function handleSubmit(event) {
-  event.preventDefault(); //기본동작을 막아줌 행동 재정의
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value)); //입력한 값을 서버로 전송
-  input.value = "";
-}
-
-function handleNickSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
+  const input = form.querySelector("input");
+  console.log(input.value);
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  }); //1. 특정한 event를emit가능하고, 2. object를 전송가능함 추가적으로 3. 서버에서 호출할 수 있는 클라이언트 function을 넣을 수 있음.
+  // 아래 설명할 것
   input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit); //버튼 초기화
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
+
+/**************************/
+/*        Websocket       */
+/**************************/
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message"); //html의 기능을 재정의
+// const socket = new WebSocket(`ws\://${window.location.host}`); //브라우저에서 지원하는 웹소켓
+// //localhost:3000 => window.location.host 모바일을 지원하기 위해 변경
+// //연결할 서버 소켓
+// //소켓으로 보낸 메시지의 성격을 구분하기 위해 이벤트명을 작성
+// function makeMessage(type, payload) {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
+//   // websocket API가 브라우저에서 제공하는 것이기 때문에, 환경을 유연하게 둬야함
+//   //응답을 받는 서버가 통일되지 않은 상태 즉, js서버가 아닐 수 있기에, string으로 보내야 처리가 가능
+// }
+// socket.addEventListener("open", () => {
+//   console.log("Connected to Server V");
+// }); //server와 연결을 시작
+// socket.addEventListener("message", (message) => {
+//   console.log(message);
+//   const li = document.createElement("li");
+//   li.innerText = message.data;
+//   messageList.append(li);
+// });
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected to Server V");
+// }); //server와 연결을 시작
+// function handleSubmit(event) {
+//   event.preventDefault(); //기본동작을 막아줌 행동 재정의
+//   const input = messageForm.querySelector("input");
+//   socket.send(makeMessage("new_message", input.value)); //입력한 값을 서버로 전송
+//   input.value = "";
+// }
+// function handleNickSubmit(event) {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   socket.send(makeMessage("nickname", input.value));
+//   input.value = "";
+// }
+// messageForm.addEventListener("submit", handleSubmit); //버튼 초기화
+// nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
+/**************************/
+/*        SocektIO        */
+/**************************/
+
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 //소켓 IO추가할것
@@ -17,35 +21,73 @@ const handleListen = () => console.log("listening on http://localhost:3000");
 // app.listen(3000, handleListen);
 //express환경에서 http와 ws를 둘다 돌리는 작업
 //필수사항은 아님 여기서는 동일한 포트에서 두가지 작업을 처리하기 위해 설정
-const server = http.createServer(app); //ws를 쓰기위해 express로부터 http서버를 생성
-const wss = new WebSocket.Server({ server }); //브라우저와 서버의 연결
+const httpServer = http.createServer(app); //ws를 쓰기위해 express로부터 http서버를 생성
+const wsServer = SocketIO(httpServer); //socket io 서버 생성
 //브라우저 상에서 websocket의 open이벤트로 감지가능
 
-//socket은 누군가와 연결될지 판단하는 연락망
-//wss의 connection 이벤트 감지
-
-const sockets = []; //서버와 연결된 브라우저 소켓들
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon"; //닉네임 초기화
-  console.log("Connected to Browser V");
-  socket.on("close", () => console.log("Disconnected from browser X")); //브라우저와의 연결이 끊어지는 이벤트체크
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg); //string을 다시 객체화
-
-    switch (message.type) {
-      case "new_message": //타입에 따라 메시지처리
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-        break;
-      case "nickname": //타입에 따라 닉네임처리
-        socket["nickname"] = message.payload; //소켓 객체에 닉네임을 추가
-        break;
-    }
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done(); //클라이언트로 부터 전달받은 함수를 호출하면 서버에서 호출하지만 실행은 클라이언트에서 됨.
+    }, 10000); // 즉 클라이언트로부터 전달받은 argument를 토대로 오래걸리는 작업을 한 뒤, 함수호출을 통해 작업종료 확인하는 등 여러 방법으로 사용가능
   });
 });
-//브라우저와 연결된 소켓
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
+
+/**************************/
+/*         Websocket      */
+/**************************/
+
+// import http from "http";
+// import WebSocket from "ws";
+// import express from "express";
+
+// //소켓 IO추가할것
+
+// const app = express();
+
+// app.set("view engine", "pug");
+// app.set("views", __dirname + "/views"); //템플릿 엔진 설정작업
+// app.use("/public", express.static(__dirname + "/public")); //유저가 볼 수 있는 범위 설정
+// app.get("/", (req, res) => res.render("home"));
+// app.get("/*", (req, res) => res.redirect("/"));
+
+// const handleListen = () => console.log("listening on http://localhost:3000");
+
+// // app.listen(3000, handleListen);
+// //express환경에서 http와 ws를 둘다 돌리는 작업
+// //필수사항은 아님 여기서는 동일한 포트에서 두가지 작업을 처리하기 위해 설정
+// const server = http.createServer(app); //ws를 쓰기위해 express로부터 http서버를 생성
+// const wss = new WebSocket.Server({ server }); //브라우저와 서버의 연결
+// //브라우저 상에서 websocket의 open이벤트로 감지가능
+
+// //socket은 누군가와 연결될지 판단하는 연락망
+// //wss의 connection 이벤트 감지
+
+// const sockets = []; //서버와 연결된 브라우저 소켓들
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon"; //닉네임 초기화
+//   console.log("Connected to Browser V");
+//   socket.on("close", () => console.log("Disconnected from browser X")); //브라우저와의 연결이 끊어지는 이벤트체크
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg); //string을 다시 객체화
+
+//     switch (message.type) {
+//       case "new_message": //타입에 따라 메시지처리
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//         break;
+//       case "nickname": //타입에 따라 닉네임처리
+//         socket["nickname"] = message.payload; //소켓 객체에 닉네임을 추가
+//         break;
+//     }
+//   });
+// });
+// //브라우저와 연결된 소켓
+
+// server.listen(3000, handleListen);
